4e0b676915f0e8bfab4a162c55c362161651d167
pablomarrero15 <105667479+pablomarrero15@users.noreply.github.com>
2022-05-16 14:40:53 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1652718288,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/suelo.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/caja-sheet0.png",
+		"images/sierra-sheet0.png",
+		"images/moneda-sheet0.png",
+		"images/colision.png",
+		"images/sprite-sheet0.png",
+		"images/saw.png",
+		"images/limite-sheet0.png",
+		"images/start-sheet0.png",
+		"images/faster-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
